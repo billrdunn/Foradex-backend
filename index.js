935c065fa4632f8321aaaea7ca8a348436c0ca2e
bill <billrdunn@gmail.com>
2022-04-11 07:03:21 +0100

    Add example for vscode REST client and add morgen middleware for logging requests.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 let items = [
   {
@@ -19,13 +20,36 @@ let items = [
   },
 ];
 
+app.use(morgan("tiny"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/items", (req, res) => {
+app.get("/api/items", (req, res) => {
+  // Send as a json fomatted string
   res.json(items);
 });
+
+app.get("/api/items/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const item = items.find((item) => item.id === id);
+  item ? res.json(item) : res.status(404).end();
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  items = items.filter((item) => item.id !== id);
+  res.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+// Use the middleware after the routes so it is
+// only called if no route handles the request
+app.use(unknownEndpoint)
 
 const PORT = 3001;
 app.listen(PORT, () => {
