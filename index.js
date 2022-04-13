f2f8329e844bccda5af5c4eb13fafecfaa9b6d5e
Bill Dunn <billrdunn@gmail.com>
2022-04-13 08:52:04 +0100

    Add eslint to project and initialise eslintrc file. Note Prettier is no longer working.

--- index.js
+++ index.js
@@ -1,57 +1,60 @@
 // Environment variables from .env file now available globally
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const Item = require("./models/item");
-const { redirect } = require("express/lib/response");
+require('dotenv').config()
+const express = require('express')
 
-app.use(cors());
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Item = require('./models/item')
+
+app.use(cors())
 
 // Check if the build dir contains a
 // file corresponding to the request's address
 // and if so, return it.
-app.use(express.static("build"));
+app.use(express.static('build'))
 app.use(express.json())
-app.use(morgan("tiny"));
+app.use(morgan('tiny'))
 // The order of middlewares matters!
 
-app.get("/api/items", (req, res) => {
-  Item.find({}).then((items) => {
-    // Send as a json fomatted string
-    res.json(items);
-  });
-});
+app.get('/api/items', (req, res) => {
+  Item.find({})
+    .then((items) => {
+      // Send as a json fomatted string
+      res.json(items)
+    })
+})
 
-app.get("/api/items/:id", (req, res, next) => {
+app.get('/api/items/:id', (req, res, next) => {
   Item.findById(req.params.id)
     .then((item) => {
-      item ? res.json(item) : res.status(404).end();
+      item ? res.json(item) : res.status(404).end()
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
 // Use the middleware after the routes so it is
 // only called if no route handles the request
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message)
 
-  if (error.name === "CastError") {
+  if (err.name === 'CastError') {
     // Status 400 means request should not be repeated without modifications
-    return res.status(400).send({ error: "malformatted id" });
+    return res.status(400).send({ error: 'malformatted id' })
   }
-};
+
+  next(err)
+}
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const { PORT } = process.env
 app.listen(PORT, () => {
-  console.log("Server running at http://localhost:" + PORT + "/");
-});
+  console.log(`Server running at http://localhost:${PORT}/`)
+})
--- models/item.js
+++ models/item.js
@@ -1,40 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
+
   .catch((error) => {
-    console.log("error connecting to MongoDB: ", error.message);
-  });
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const itemSchema = new mongoose.Schema({
-    latin: {
-        type: String,
-        minLength: 6,
-        required: true
-    },
-    common: {
-        type: [String],
-        required: true
-    }
-});
+  latin: {
+    type: String,
+    minLength: 6,
+    required: true,
+  },
+  common: {
+    type: [String],
+    required: true,
+  },
+})
 
 // Format object returned by Mongoose by
 // modifying toJSON method of schema
-itemSchema.set("toJSON", {
+itemSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     // transform _id (object) to id (string)
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
     // don't need mongo versioning field at frontend
-    delete returnedObject.__v;
+    delete returnedObject.__v
   },
-});
+})
 
 module.exports = mongoose.model('Item', itemSchema)
