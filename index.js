25eeead297d3e0949759989d9e0d33aab590f0a2
Bill Dunn <billrdunn@gmail.com>
2022-04-13 06:55:29 +0100

    Modify backend to retrieve data from MongoDB server. Use Mongoose to set Item schemas and retrieve items from the database. Add error handling to prevent crashes when attempting to access an item with a malformatted id.

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
+// Environment variables from .env file now available globally
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-
-let items = [
-  {
-    latin: "Amanita muscara",
-    common: ["Fly agaric"],
-    id: 0,
-  },
-  {
-    latin: "Scleroderma citrinum",
-    common: ["Common earthball"],
-    id: 1,
-  },
-  {
-    latin: "Amanita virosa",
-    common: ["Destroying angel"],
-    id: 2,
-  },
-  {
-    latin: "Auricularia auricula-judae",
-    common: ["Jelly Ears", "Wood Ears"],
-    id: 3,
-  },
-
-];
+const Item = require("./models/item");
+const { redirect } = require("express/lib/response");
 
 app.use(cors());
 app.use(morgan("tiny"));
@@ -33,34 +13,36 @@ app.use(morgan("tiny"));
 // Check if the build dir contains a
 // file corresponding to the request's address
 // and if so, return it.
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 app.get("/api/items", (req, res) => {
-  // Send as a json fomatted string
-  res.json(items);
+  Item.find({}).then((items) => {
+    // Send as a json fomatted string
+    res.json(items);
+  });
 });
 
 app.get("/api/items/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const item = items.find((item) => item.id === id);
-  item ? res.json(item) : res.status(404).end();
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  items = items.filter((item) => item.id !== id);
-  res.status(204).end();
+  Item.findById(req.params.id)
+    .then((item) => {
+      item ? res.json(item) : res.status(404).end();
+    })
+    .catch((error) => {
+      console.log(error);
+      // Status 400 means request should not be repeated without modifications
+      res.status(400).send({ error: "malformatted id" });
+    });
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 // Use the middleware after the routes so it is
 // only called if no route handles the request
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server running at http://localhost:" + PORT + "/");
 });
---
+++ models/item.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB: ", error.message);
+  });
+
+const itemSchema = new mongoose.Schema({
+  latin: String,
+  common: [String],
+});
+
+// Format object returned by Mongoose by
+// modifying toJSON method of schema
+itemSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    // transform _id (object) to id (string)
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    // don't need mongo versioning field at frontend
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Item', itemSchema)
