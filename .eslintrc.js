57ffd057fd8d2c18168456a0fb8eaeda9e6464c1
Bill Dunn <billrdunn@gmail.com>
2022-04-14 07:43:24 +0100

    Use prettier and eslint and enforce airbnb style.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,12 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 'latest'
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "no-console": 0,
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 0
-
-  }
-}
+};
--- index.js
+++ index.js
@@ -1,60 +1,58 @@
 // Environment variables from .env file now available globally
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 
-const app = express()
-const morgan = require('morgan')
-const cors = require('cors')
-const Item = require('./models/item')
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const Item = require("./models/item");
 
-app.use(cors())
+app.use(cors());
 
-// Check if the build dir contains a
-// file corresponding to the request's address
-// and if so, return it.
-app.use(express.static('build'))
-app.use(express.json())
-app.use(morgan('tiny'))
+// Check if the build dir contains a file corresponding to the request's address and if so, return it.
+app.use(express.static("build"));
+app.use(express.json());
+app.use(morgan("tiny"));
 // The order of middlewares matters!
 
-app.get('/api/items', (req, res) => {
-  Item.find({})
-    .then((items) => {
-      // Send as a json fomatted string
-      res.json(items)
-    })
-})
+app.get("/api/items", (req, res) => {
+  Item.find({}).then((items) => {
+    // Send as a json fomatted string
+    res.json(items);
+  });
+});
 
-app.get('/api/items/:id', (req, res, next) => {
+app.get("/api/items/:id", (req, res, next) => {
   Item.findById(req.params.id)
     .then((item) => {
-      item ? res.json(item) : res.status(404).end()
+      if (item) res.json(item);
+      else res.status(404).end();
     })
-    .catch((err) => next(err))
-})
+    .catch((err) => next(err));
+});
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-}
+  res.status(404).send({ error: "unknown endpoint" });
+};
 
 // Use the middleware after the routes so it is
 // only called if no route handles the request
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const errorHandler = (err, req, res, next) => {
-  console.error(err.message)
+  console.error(err.message);
 
-  if (err.name === 'CastError') {
+  if (err.name === "CastError") {
     // Status 400 means request should not be repeated without modifications
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).send({ error: "malformatted id" });
   }
+  next(err);
+  return null;
+};
 
-  next(err)
-}
+app.use(errorHandler);
 
-app.use(errorHandler)
-
-const { PORT } = process.env
+const { PORT } = process.env;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`)
-})
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
