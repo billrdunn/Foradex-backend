f54f79b8b07ea3acb9131a225cede7cb2d39ea28
Bill Dunn <billrdunn@gmail.com>
2022-04-20 11:19:24 +0100

    Start refactoring to use async/await. Add some API tests to prevent regression during refactoring.

--- controllers/items.js
+++ controllers/items.js
@@ -1,20 +1,25 @@
 const itemRouter = require("express").Router();
 const Item = require("../models/item");
 
-itemRouter.get("/", (req, res) => {
-  Item.find({}).then((items) => {
-    // Send as a json fomatted string
-    res.json(items);
-  });
+itemRouter.get("/", async (req, res) => {
+  const items = await Item.find({});
+  // Send as a json fomatted string
+  res.json(items);
 });
 
-itemRouter.get("/:id", (req, res, next) => {
-  Item.findById(req.params.id)
-    .then((item) => {
-      if (item) res.json(item);
-      else res.status(404).end();
-    })
-    .catch((err) => next(err));
+itemRouter.get("/:id", async (req, res, next) => {
+  const item = await Item.findById(req.params.id);
+  if (item) res.json(item);
+  else res.status(404).end();
+
+  // what about catch?
+  
+  // Item.findById(req.params.id)
+  //   .then((item) => {
+  //     if (item) res.json(item);
+  //     else res.status(404).end();
+  //   })
+  //   .catch((err) => next(err));
 });
 
 module.exports = itemRouter;
--- tests/item_api.test.js
+++ tests/item_api.test.js
@@ -1,29 +1,17 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
 const Item = require("../models/item");
 
-const initialItems = [
-  {
-    _id: "625fc3035965f5afa2a08e06",
-    latin: "Marasmius oreades",
-    common: ["Fairy ring mushroom", "Fairy ring champignon"],
-    __v: 0,
-  },
-  {
-    _id: "625fc34d5965f5afa2a08e07",
-    latin: "Lycoperdon utriformis",
-    common: ["Mosaic puffball"],
-    __v: 0,
-  },
-];
+
 
 // Before each test, clear the database
 beforeEach(async () => {
   await Item.deleteMany({});
-  let itemObject = new Item(initialItems[0]);
+  let itemObject = new Item(helper.initialItems[0]);
   await itemObject.save();
-  itemObject = new Item(initialItems[1]);
+  itemObject = new Item(helper.initialItems[1]);
   await itemObject.save();
 });
 
@@ -39,13 +27,11 @@ test("items are returned as json", async () => {
 
 test("all items are returned", async () => {
   const response = await api.get("/api/items");
-
-  expect(response.body).toHaveLength(initialItems.length);
+  expect(response.body).toHaveLength(helper.initialItems.length);
 });
 
 test("the second item is mosaic puffball", async () => {
   const response = await api.get("/api/items");
-
   expect(response.body[1].latin).toEqual("Lycoperdon utriformis");
 });
 
@@ -53,9 +39,19 @@ test("the items contain Marasmius oreades", async () => {
   const response = await api.get("/api/items");
 
   // Create an array containing the latin name of every item returned by the API
-  const contents = response.body.map((item) => item.latin);
+  const latinNames = response.body.map((item) => item.latin);
 
-  expect(contents).toContain("Marasmius oreades");
+  expect(latinNames).toContain("Marasmius oreades");
+});
+
+test("a GET request with the appropriate id returns the corresponding item", async () => {
+  const response = await api.get("/api/items/625fc34d5965f5afa2a08e07");
+
+  expect(response.body.latin).toEqual(helper.initialItems[1].latin);
+});
+
+test("a GET request to an unknown item responds with status 404 ", async () => {
+  await api.get("/api/items/525fc34d5965f5afa2a08e07").expect(404);
 });
 
 afterAll(() => {
